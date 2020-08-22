4ec5dd7934f226d9255a004a99b2ae7e175b571b
ProjectHSI <57279339+ProjectHSI@users.noreply.github.com>
2020-08-22 18:04:32 +1000

    Updating. Adding actual download and install code now.

--- bin/hpmmodule.js
+++ bin/hpmmodule.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
 
 const npmlog = require('npmlog');
-npmlog.info("HPM","Welcome to HPM")
+npmlog.info("HPM","Welcome to HPM!")
+npmlog.info("HPM","It worked if it ends with ok.")
