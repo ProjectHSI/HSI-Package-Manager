4c2a3a236931da672e9f863f7c45a2a5b49835d3
ProjectHSI <57279339+ProjectHSI@users.noreply.github.com>
2020-08-22 18:41:17 +1000

    e

--- bin/hpmmodule.js
+++ bin/hpmmodule.js
@@ -3,19 +3,16 @@
 const npmlog = require('npmlog');
 npmlog.info("HPM","Welcome to HPM!")
 npmlog.info("HPM","It worked if it ends with ok.")
+npmlog.info("HPM",process.argv)
 switch (process.argv[2]) {
     case "install":
         npmlog.info("HPM","You are installing from the public REPO.")
         npmlog.info("HPM","Remember to put the install location after the Add-on/Module you which to install.")
-        switch (process.args[4]) {
-            case null:
-                npmlog.warn("HPM","WARNING! You are using the command WITH OUT A PATH IDENTIFER OR THE PATH YOU ENTERED IS NULL OR UNDEFINED.\nUsing the Directory of the command you are running.")
-                npmlog.warn("HPM", process.argv)
-                break;
-            case undefined:
-                npmlog.warn("HPM","WARNING! You are using the command WITH OUT A PATH IDENTIFER OR THE PATH YOU ENTERED IS NULL OR UNDEFINED.\nUsing the Directory of the command you are running.")
-                npmlog.warn("HPM", process.argv)
-                break;
+        npmlog.info("HPM",`Installing ${process.argv[3]} If it exists in the Repo.`)
+        npmlog.info("HPM",`You may see extra logs info if you activated --verbose.`)
+        switch (process.argv[3]) {
+            case "DroppyAddon":
+
         }
         break;
 }
