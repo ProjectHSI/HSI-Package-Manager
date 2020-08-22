1f41301d73375d031af49246997a47a91257e2b0
ProjectHSI <57279339+ProjectHSI@users.noreply.github.com>
2020-08-22 18:06:42 +1000

    Updated [Testing out arg detection]

--- bin/hpmmodule.js
+++ bin/hpmmodule.js
@@ -3,3 +3,7 @@
 const npmlog = require('npmlog');
 npmlog.info("HPM","Welcome to HPM!")
 npmlog.info("HPM","It worked if it ends with ok.")
+switch (process.argv[2]) {
+    case "test":
+        npmlog.notice("HPM","Test")
+}
