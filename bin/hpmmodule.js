8863b459711dde8b8d1da5e0171378e856c90878
ProjectHSI <57279339+ProjectHSI@users.noreply.github.com>
2020-09-11 18:53:37 +1000

    e

--- bin/hpmmodule.js
+++ bin/hpmmodule.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const npmlog = require('npmlog');
+npmlog.level = -Infinity
 npmlog.info("HPM", "Welcome to HPM!")
 npmlog.info("HPM", "It worked if it ends with ok.")
 npmlog.info("HPM", process.argv)
